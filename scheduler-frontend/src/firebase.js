53697f3e1fd973224c61356446bce7826f95acf3
Chen-Connie <89173725+Chen-Connie@users.noreply.github.com>
2025-04-26 18:29:37 -0700

    Add files via upload

---
+++ scheduler-frontend/src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCoOw8s4mD541LGgT4n7idQHgyY2is3BIk",
+    authDomain: "task-planner-75366.firebaseapp.com",
+    projectId: "task-planner-75366",
+    storageBucket: "task-planner-75366.firebasestorage.app",
+    messagingSenderId: "603927987249",
+    appId: "1:603927987249:web:0b8c0164cc612e68630110",
+    measurementId: "G-8XPQLRVRY8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export { auth };
+export default app;
